d4cdc04a9aba3abc30d576328cf198325a112838
Your Name <rishirk0422@gmail.com>
2026-05-26 11:10:28 +0530

    pool changes

--- routes/masters/category.routes.js
+++ routes/masters/category.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { pool } from "../config/pgdb.js";
+import { pool } from "../../config/pgdb.js"
 import { protect } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
