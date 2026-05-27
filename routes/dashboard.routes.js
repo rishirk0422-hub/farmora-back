4a6e00ae36c415a0a79661dd2887a23a05bd2acb
Your Name <rishirk0422@gmail.com>
2026-05-27 11:14:10 +0530

    Dashboard Changes

--- routes/dashboard.routes.js
+++ routes/dashboard.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { getDashboardStats } from "../controllers/dashboard.controller.js"; 
-import { protect } from "../middlewares/auth.middleware.js
+import { protect } from "../middlewares/auth.middleware.js"
 
 const router = express.Router();
 
