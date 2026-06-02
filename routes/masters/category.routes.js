1f08e4d1094e64e683662c4de8d670c09ef50115
Your Name <rishirk0422@gmail.com>
2026-06-02 14:10:40 +0530

    Update category.routes.js

--- routes/masters/category.routes.js
+++ routes/masters/category.routes.js
@@ -24,6 +24,7 @@ export const initCategoryTable = async () => {
 router.get("/", protect, async (req, res) => {
   try {
     const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
 
     const offset = (page - 1) * limit;
 
